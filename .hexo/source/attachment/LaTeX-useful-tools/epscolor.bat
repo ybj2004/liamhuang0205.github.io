6b80f8628a56f90567199b6b7c3662c285afeed8
Liam Huang <liamhuang0205+git@gmail.com>
2014-08-27 11:34:49 +0800

    site update: 2014-8-27 11:35:01:x

--- .hexo/source/attachment/LaTeX-useful-tools/epscolor.bat
+++ .hexo/source/attachment/LaTeX-useful-tools/epscolor.bat
@@ -1,18 +1,6 @@
 @echo off
 
-for /f %%i in ('dir /b *.jpg') do (
-    @echo %%i
-    bmeps -c %%i %%~ni.eps
-    @echo Finished
-    )
-
-for /f %%i in ('dir /b *.bmp') do (
-    @echo %%i
-    bmeps -c %%i %%~ni.eps
-    @echo Finished
-    )
-
-for /f %%i in ('dir /b *.png') do (
+for /f %%i in ('dir /b *.jpg *.jpeg *.bmp *.png') do (
     @echo %%i
     bmeps -c %%i %%~ni.eps
     @echo Finished
