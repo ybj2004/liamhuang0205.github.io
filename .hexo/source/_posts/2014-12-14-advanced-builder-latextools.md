887cfcc540906f1f943fb010707618c176c87634
Liam Huang <liamhuang0205@gmail.com>
2014-12-14 18:37:23 +0800

    Site updated: 2014-12-14 18:37:23

---
+++ .hexo/source/_posts/2014-12-14-advanced-builder-latextools.md
@@ -0,0 +1,89 @@
+title: 订制 Sublime Text 下 LaTeXTools 插件的编译脚本
+date: 2014-12-14 18:15:34
+categories: LaTeX
+tags: [LaTeXTools, Python, Compile]
+---
+
+[LaTeXTools](https://github.com/SublimeText/LaTeXTools) 是 [Sublime Text](http://www.sublimetext.com) 上的一个插件，用以增强在 Sublime Text 上编辑和编译 LaTeX 文档的体验。但是其自带的编译脚本比较难用，缺点如下：
+
+* 调用 LaTeXmk 或者 TeXify，多次编译，速度缓慢，难以切换。
+* 无法在编译时方便地添加命令行参数，导致依赖 `\write18` 命令的宏包（比如 `minted`）无法使用。
+
+为了解决这个问题，我自己写了一个编译脚本，命名为 `advancedBuilder.py`。
+
+<!--more-->
+
+## 安装方法
+
+* 在[这里]({{site.url}}/attachment/LaTeX-useful-tools/advancedBuilder.py)下载编译脚本；
+* 打开 Sublime Text 目录；
+* 将脚本拷贝至 `./Packages/LaTeXTools/builders/`；
+* 编辑 `./Packages/User/LaTeXTools.sublime-settings`，将 `"builder"` 值改为 `advanced`：`"builder": "advanced"`；
+* 重启 Sublime Text。
+
+## 使用方法
+
+基本的使用方法和原本的 `traditionalBuilder.py` 相同，即使用快捷键 `Ctrl + B` 编译（Mac OS X 是 `CMD + B`）。但在文档开头的注释中，增加了更多的用法。
+
+### 改变编译流程
+
+可以在主文档开头的注释中，使用下列语法改变编译流程。
+
+{% code demo lang:tex %}
+%!TEX builder = BUILDER
+{% endcode %}
+
+其中 `BUILDER` 的可选值有：
+
+* `SIMPLE`
+* `LATEXMK`
+* `TEXIFY`
+
+大小写不论，默认是 `SIMPLE`。
+
+### 选择编译引擎
+
+> 此功能继承自默认编译脚本，有增强。
+
+可以在主文档开头的注释中，使用下列语法改变编译引擎。
+
+{% code demo lang:tex %}
+%!TEX program = PROGRAM
+{% endcode %}
+
+其中 `PROGRAM` 的可选值有：
+
+* `pdflatex`
+* `xelatex`
+* `lualatex`
+* `platex-ng`
+* `pdftex`
+* `xetex`
+* `luatex`
+* `ptex-ng`
+
+大小写敏感，默认值是 `pdflatex`。
+
+### 选择编译参数
+
+可以在主文档开头的注释中，使用下列语法添加编译参数。
+
+{% code demo lang:tex %}
+%!TEX option = OPTION
+{% endcode %}
+
+其中 `OPTION` 的可选值为对 `PROGRAM` 合法的任意参数。大小写敏感，默认为空。
+
+另外，强制启动 `-interaction=nonstopmode` 和 `-synctex=1` 两个参数，不允许更改。
+
+### 指定主文档
+
+> 此功能完整继承自默认编译脚本。
+
+可以在子文档的开头注释中，使用下列语法指定主文档。
+
+{% code demo lang:tex %}
+%!TEX root = ROOT
+{% endcode %}
+
+其中 `ROOT` 为主文档相对当前子文档的路径。指定主文档之后，可以在子文档中直接使用快捷键编译主文档，而不用将 Sublime Text 标签页切换到主文档再进行编译。
---
+++ attachment/LaTeX-useful-tools/advancedBuilder.py
@@ -0,0 +1,123 @@
+# ST2/ST3 compat
+from __future__ import print_function
+import sublime
+if sublime.version() < '3000':
+    # we are on ST2 and Python 2.X
+    _ST3 = False
+else:
+    _ST3 = True
+
+import sublime_plugin
+import re
+import os, os.path
+import codecs
+
+DEBUG = False
+
+DEFAULT_COMMAND_SIMPLE = ["%E",
+    "-interaction=nonstopmode -synctex=1", "OPTION"]
+
+DEFAULT_COMMAND_LATEXMK = ["latexmk",
+    "-cd", "-e",
+    "$pdflatex = '%E -interaction=nonstopmode -synctex=1 OPTION %S %O'",
+    "-f", "-pdf"]
+
+DEFAULT_COMMAND_WINDOWS_MIKTEX = ["texify",
+    "-b", "-p",
+    "--tex-option=\"--synctex=1\"", "OPTION"]
+
+class PdfBuilder(object):
+    """Base class for build engines"""
+    def __init__(self, tex_root, output, builder_settings, platform_settings):
+        self.tex_root = tex_root
+        self.tex_dir, self.tex_name = os.path.split(tex_root)
+        self.base_name, self.tex_ext = os.path.splitext(self.tex_name)
+        self.output_callable = output
+        self.out = ""
+        self.builder_settings = builder_settings
+        self.platform_settings = platform_settings
+    def display(self, data):
+        self.output_callable(data)
+    def set_output(self, out):
+        if DEBUG:
+            print("Setting out")
+            print(out)
+        self.out = out
+    def commands(self):
+        pass
+    def cleantemps(self):
+        return False
+
+class AdvancedBuilder(PdfBuilder):
+    def __init__(self, tex_root, output, builder_settings, platform_settings):
+        # Sets the file name parts, plus internal stuff
+        super(AdvancedBuilder, self).__init__(tex_root, output, builder_settings, platform_settings)
+        self.name = "Advanced Builder"
+        # Display output?
+        self.display_log = builder_settings.get("display_log", False)
+        self.engine = 'pdflatex'
+        self.option = ""
+    def commands(self):
+        # print greeting
+        self.display("\n\n")
+        self.display("This is the Advanced Bulder.")
+        self.display("\n")
+        # copy vars
+        engine = self.engine
+        option = self.option
+        tex_name = self.tex_name
+        builder_name = "SIMPLE"
+        # check root file, settings in the comments
+        for line in codecs.open(self.tex_root, "r", "UTF-8", "ignore").readlines():
+            if not line.startswith("%"):
+                # if not comment line, break
+                break
+            else:
+                # process the comment lines
+                mEng = re.match(r"%\s*![Tt][Ee][Xx]\s+(?:TS-)?program *= *(xelatex|lualatex|pdflatex|xetex|luatex|pdftex|platex-ng|ptex-ng)\s*$",line)
+                mOpt = re.match(r"%\s*![Tt][Ee][Xx]\s+(?:TS-)?option *= *(.*)\s*?$",line)
+                mBui = re.match(r"%\s*![Tt][Ee][Xx]\s+(?:TS-)?builder *= *([Ss][Ii][Mm][Pp][Ll][Ee]|[Ll][Aa][Tt][Ee][Xx][Mm][Kk]|[Tt][Ee][Xx][Ii][Ff][Yy])\s*$",line)
+                # The engine
+                if mEng: # is not None
+                    engine = mEng.group(1)
+                # The option
+                if mOpt: # is not None
+                    option = mOpt.group(1)
+                if mBui:
+                    builder_name = mBui.group(1).upper()
+        if builder_name == "LATEXMK":
+            cmd = DEFAULT_COMMAND_LATEXMK[:]
+        elif builder_name == "TEXIFY":
+            cmd = DEFAULT_COMMAND_WINDOWS_MIKTEX[:]
+        else:
+            cmd = DEFAULT_COMMAND_SIMPLE[:]
+
+        # apply the settings
+        if 'latexmk' == cmd[0]:
+            cmd[3] = cmd[3].replace("%E", engine).replace("OPTION", option)
+        elif 'texify' == cmd[0]:
+            if engine != self.engine:
+                sublime.error_message("Sorry, cannot select engine using a %!TEX program directive on MikTeX.\n")
+                yield("", "Could not compile.")
+            if option != self.option:
+                cmd[4] = "--tex-option=\"%s\"" % option
+            else:
+                cmd[4] = ""
+        else: # SIMPLE
+            cmd[0] = engine
+            cmd[2] = option
+        # print the whole command
+        self.display("Command Line:\n")
+        for element in cmd + [tex_name]:
+            self.display(element + " ")
+        else:
+            self.display("\n")
+        # texify wants the .tex extension; latexmk doesn't care either way
+        yield (cmd + [tex_name], "")
+        self.display("Done.\n")
+
+        # This is for debugging purposes
+        if self.display_log:
+            self.display("\nCommand results:\n")
+            self.display(self.out)
+            self.display("\n\n")
